8375b7f4d710661a9c4cd77ffb11b2da1b5f555c
coljacob <coline1996@gmail.com>
2016-04-24 22:00:56 +0200

    coins and hero

--- src/model/character/Hero.java
+++ src/model/character/Hero.java
@@ -1,8 +1,26 @@
 package model.character;
 
+import model.item.Coin;// sinon ça bugg
+
 public class Hero extends Player{
-	
+	private int coinsNumber;
+	private int maxCoinsNumber;
 	public Hero(int posX,int posY, int attackRange){
 		super(posX, posY, attackRange);
+		this.coinsNumber=0;
+		this.maxCoinsNumber=50;
+	}
+	public int getCoinsNumber(){
+		return this.coinsNumber;
+	}
+	public int getMaxCoinsNumber(){
+		return this.maxCoinsNumber;
+	}
+	
+	public void grabCoin (Coin coin){
+		if(this.getPosX()==coin.getPosX()&& this.getPosY()==coin.getPosY()){
+			this.coinsNumber+=1;
+		}
+		
 	}
 }
