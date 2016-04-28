ea0cb44f7976b0de56a61fc34aeacdc9ec9f5244
coljacob <coline1996@gmail.com>
2016-04-28 14:35:59 +0200

    MultiPanel (Split)

--- src/view/Window.java
+++ src/view/Window.java
@@ -3,25 +3,36 @@ import java.awt.Color;
 import java.awt.event.KeyListener;
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JSplitPane;
+
 import view.Inventory; 
 import view.HeroHealth;
 
 
 public class Window {
-
+private JSplitPane split1, split2;
 private Map map = new Map();// cette ligne pose problème pour les labels!!
 private Inventory inventory= new Inventory();
 private HeroHealth health= new HeroHealth();	
 	public Window(){	    
 	    JFrame window = new JFrame("Game");
 	    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    window.setBounds(0, 0, 1000, 1020);
+	    window.setBounds(0, 0, 1500, 1020);
 	    window.getContentPane().setBackground(Color.gray);
-	    window.getContentPane().add(this.map,BorderLayout.CENTER);
-	    window.getContentPane().add(this.inventory,BorderLayout.EAST);
-	    window.getContentPane().add(this.health,BorderLayout.EAST);
+	    
+	    split1 = new JSplitPane(JSplitPane.VERTICAL_SPLIT, health,inventory);
+	    //On place le premier séparateur
+	    split1.setDividerLocation(300);
+	    
+	    split2 = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,map, split1);
+	    //On place le deuxième séparateur
+	    split2.setDividerLocation(1000);
+	    
+	    window.getContentPane().add(split2,BorderLayout.CENTER);
+	    
 	    window.setVisible(true);
 	    window.setResizable(true);
+	   
 	    
 	}	
 	
