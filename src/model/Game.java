21533ee19359a92f399bdfe64290b31977f46d18
coljacob <coline1996@gmail.com>
2016-04-30 21:03:53 +0200

    teleportation (fonctionne)

--- src/model/Game.java
+++ src/model/Game.java
@@ -10,12 +10,14 @@ import model.item.*;
 public class Game {
 	private ArrayList<Wall> walls = new ArrayList<Wall>();// Liste des murs
 	private ArrayList<Tile> tiles= new ArrayList<Tile>();// liste des cases (normales ici)
+	private ArrayList<Tile> teleportationTiles= new ArrayList<Tile>();// TODO teleportation du hero
 	private ArrayList<Coin> coinsOnFloor = new ArrayList<Coin>();
 	private ArrayList<Potion> potions =new ArrayList<Potion>();
 	private Hero hero;
 	private ArrayList<PNJ> PNJs = new ArrayList<PNJ>();
 	private Window window;
 	private int size = 20;
+	private int teleNum=5;
 	private int PNJNumber=5;
 	private int coinNumber=6;
 	private int potionNumber=8;
@@ -59,35 +61,50 @@ public class Game {
 				tiles.add(new Tile(i,j));// les cases
 			}
 		}
-		// Creating Hero at  position 
 		
-		hero= new Hero(10,10,1);
+		//creating teleportation tiles
+		for (int i=0; i<teleNum; i++){
+			int tX = randomNum(1, size - 2);
+			int tY = randomNum(1, size - 2);
+			teleportationTiles.add(new Tile(tX,tY));
+		}
+		//hero= new Hero(10,10,1);
+		// Creating hero
+		int hX=randomNum(1,size-2);
+		int hY=randomNum(1,size-2);
+		while (beOnTeleTile(hX,hY)==true){
+			hX=randomNum(1,size-2);
+			hY=randomNum(1,size-2);		
+		}
+		hero=new Hero(hX,hY,1);
 		// Creating PNJ's
 		for (int i=0; i<PNJNumber; i++){
 			int posX = randomNum(1, size - 2);
 			int posY = randomNum(1, size - 2);
 			
-			while(Collision(posX, posY)==true){
+			while(Collision(posX, posY)==true && beOnTeleTile(posX,posY)==true){
 				posX = randomNum(1,size-2);
 				posY = randomNum(1,size-2);
 			}
 			PNJs.add(new PNJ(posX,posY,1,1));
 		}
+		// Creation des pieces
 		for (int i=0; i<coinNumber; i++){
 			int cX = randomNum(1, size - 2);
 			int cY = randomNum(1, size - 2);
 			
-			while(Collision(cX, cY)==true){
+			while(Collision(cX, cY)==true && beOnTeleTile(cX,cY)==true){
 				cX = randomNum(1,size-2);
 				cY = randomNum(1,size-2);
 			}
 			coinsOnFloor.add(new Coin(cX,cY));
 		}
+		//Creation des potions
 		for (int i=0; i<potionNumber; i++){
 			int pX = randomNum(1, size - 2);
 			int pY = randomNum(1, size - 2);
 			
-			while(Collision(pX, pY)==true){
+			while(Collision(pX, pY)==true && beOnTeleTile(pX,pY)==true){
 				pX = randomNum(1,size-2);
 				pY = randomNum(1,size-2);
 			}
@@ -129,6 +146,15 @@ public class Game {
 		
 		return res;
 	}
+	public boolean beOnTeleTile(int x, int y){
+		boolean res=false;
+		for (Tile tile: teleportationTiles){
+			if (tile.getPosX()==x && tile.getPosY()==y){
+				res=true;
+			}
+		}
+		return res;
+	}
 	
 	//retourne un nombre aleatoire
 	public int randomNum(int min, int max){
@@ -136,17 +162,7 @@ public class Game {
 		return nb;
 	}
 	
-	//renvoie l' indice d' une pièce  dans la liste sachant sa  position.
 	
-	public int listIndexCoin(ArrayList<Coin> list,int x,int y){
-		int res=0;
-		for(int i=0; i<list.size();i++){
-			if(list.get(i).getPosX()==x && list.get(i).getPosY()==y){
-				res=i;
-			}
-		}
-		return res;
-	}
 	//revoie l' indice d' un ennemi dans la liste et sachant sa position
 	public int listIndexPNJ(ArrayList<PNJ> list,int x,int y){
 		int range= hero.getAttackRange();
@@ -163,6 +179,7 @@ public class Game {
 		if (Collision(hero.getPosX() - 1, hero.getPosY()) == false){
 			hero.move(-1, 0);
 			takeCoin();
+			teleportation();
 			window.draw(this.getMap());
 			window.uploadCoins(this.getCoinsNumber());
 		}
@@ -171,6 +188,7 @@ public class Game {
 		if (Collision(hero.getPosX() + 1, hero.getPosY()) == false){
 			hero.move(1,0);
 			takeCoin();
+			teleportation();
 			window.draw(this.getMap());
 			window.uploadCoins(this.getCoinsNumber());
 		}
@@ -179,6 +197,7 @@ public class Game {
 		if (Collision(hero.getPosX(), hero.getPosY() + 1) == false){
 			hero.move(0,1);
 			takeCoin();
+			teleportation();
 			window.draw(this.getMap());
 			window.uploadCoins(this.getCoinsNumber());
 		}
@@ -187,9 +206,24 @@ public class Game {
 		if (Collision(hero.getPosX(), hero.getPosY() - 1) == false){
 			hero.move(0,-1);
 			takeCoin();
+			teleportation();
 			window.draw(this.getMap());
 			window.uploadCoins(this.getCoinsNumber());
 		}
+	}
+	public int randomTeleTile(){
+		int index =randomNum(0,teleNum-1);
+		return index;
+	}
+	public void teleportation(){
+		int x=hero.getPosX();
+		int y= hero.getPosY();
+		if(beOnTeleTile(x,y)==true){
+			int i=randomTeleTile();
+			hero.setPosX(teleportationTiles.get(i).getPosX());
+			hero.setPosY(teleportationTiles.get(i).getPosY());
+		}
+		
 	}
 	
 	public void heroAttacks(){
@@ -262,6 +296,12 @@ public class Game {
 			int y = wall.getPosY();
 			map[x][y] = 1;
 		}
+		for(Tile teleTile: teleportationTiles){
+			int x = teleTile.getPosX();
+			int y = teleTile.getPosY();
+			map[x][y] = 6;
+		}
+		
 		for(Coin coin: coinsOnFloor){
 			int x = coin.getPosX();
 			int y = coin.getPosY();
--- src/view/Map.java
+++ src/view/Map.java
@@ -22,6 +22,7 @@ public class Map extends JPanel {
 	private BufferedImage tileImage;
 	private BufferedImage ratImage;
 	private BufferedImage potionImage;
+	private BufferedImage teleTileImage;
 	
 	
 	/*CONSTRUCTEUR*/
@@ -36,6 +37,7 @@ public class Map extends JPanel {
 			tileImage =ImageIO.read(new File("/Users/coline/Documents/GitHub/Dungeon/Dungeon/src/images/tile.png"));
 			ratImage  =ImageIO.read(new File("/Users/coline/Documents/GitHub/Dungeon/Dungeon/src/images/rat.png"));
 			potionImage =ImageIO.read(new File("/Users/coline/Documents/GitHub/Dungeon/Dungeon/src/images/potion.png"));
+			teleTileImage =ImageIO.read(new File("/Users/coline/Documents/GitHub/Dungeon/Dungeon/src/images/teletile.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -110,6 +112,14 @@ public class Map extends JPanel {
 							//final BufferedImage image = ImageIO.read(new File("/Users/teddybilba/Documents/Teddy/ULB/BA2/Informatique/Donjon/Donjon/src/View/goku.png"));
 							
 							g.drawImage(potionImage, x*50, y*50, 48, 48,  null);
+						}
+						catch(Exception e){}
+					}
+					else if(color == 6){
+						try{
+							//final BufferedImage image = ImageIO.read(new File("/Users/teddybilba/Documents/Teddy/ULB/BA2/Informatique/Donjon/Donjon/src/View/goku.png"));
+							
+							g.drawImage(teleTileImage, x*50, y*50, 48, 48,  null);
 						}
 						catch(Exception e){}
 					}
